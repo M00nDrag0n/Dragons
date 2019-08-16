9faa738c901771ad03def5c318664ff6aaee9534
Student <example@example.com>
2019-08-16 11:06:10 -0700

    finish referencing basic JavaScript stucture in scripts.js file, remove text from wrong reference code, start messing with the styles.css file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,40 @@
 $(document).ready(function(){
-  function getSelectedOptions(sel, fn) {
-    var opts = [], opt;
+  $("#PersonalityQuiz").submit(function(event) {
+
+    event.preventDefault();
+
+    var answers=$(this).serializeArray();
+
+    var scores = {
+      ["result value"]: 0;
+      ["result value"]: 0;
+      ["result value"]: 0;
+      ["result value"]: 0;
+      ["result value"]: 0;
+      ["result value"]: 0;
+
+  };
+
+  for (var answer of answers){
+    scores[answer.value] +=1;
   }
 
+  //console.log(scores);
+  for(var dragon in scores){
+    console.log(scores[dragon]);
+
+  }
+
+  var maxDragon = ["result value"]; //this refers to maximum score in result totals
+
+  for(var dragon in scores){
+    if scores[dragon] > scores[maxDragon]){
+      maxDragon= dragon;
+    }
+    $("#personalityQuiz").css('display', 'none');
+    $(".result#"+maxDragon).css('display', 'block');
+  }
 
 });
+
+})
